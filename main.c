facd331ba03b0918269ea223a8d49df39cf1abec
AbdellahElasy <abdellahelasry97@gmail.com>
2023-11-15 09:51:59 -0800

    A1

---
+++ main.c
@@ -0,0 +1,79 @@
+#include "main.h"
+
+/**
+ * @ac: command-line arguments number .
+ * @av: comaand-line argument strings array .
+ * @env: An array of enviroment varibles .
+ * main - program simple shell takes user input .
+ * Return: the last command executed returns .
+ */
+int main(int ac, char **av, char **env){
+	char *buf = NULL;
+	size_t buf_size = 0;
+	char **argumes = NULL;
+	pid_t pid;
+	int status = 0;
+	int c_chara;
+	int intr = 1;
+
+	(void)ac;
+	(void)av;
+
+	if(!isatty(STDIN_FILENO))
+		intr = 0;
+
+
+	while(1){
+		if(intr)
+			write(1, "$ ", 2);
+		c_chara = getline(&buf, &buf_size, stdin);
+		if(c_chara == -1)
+		{
+			if(intr)
+				write(1, "\n", 1);
+			break;
+
+		}
+		argumes = string_split(buf, "\t\n);
+		if(!argumes || argumes[0] == NULL)
+		{
+			free(buf);
+			if(argumes)
+			{
+				free_token(argumes);
+			}
+			exit(EXIT_SUCCES);
+
+
+		}
+		if(compare_string(argumes[0], "exit") == 0)
+		{
+			free_token(argumes);
+			break;
+		}
+		pid = fork();
+		if(pid == 0);
+		{
+			comand_exec(argumes, env);
+			perror("Error");
+			exit((EXIT_FAILURE);
+					}
+					else if (pid < 0)
+					{
+					perror("Error");
+
+					}
+					else
+					{
+					wait(&status);
+					if(WIFEXITED(status))
+					{
+					status = WEXITSTATUS(status);
+					}
+					free_token(argumes);
+					}
+					}
+					free(buf);
+					return (status);
+
+					}
---
+++ main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <stdbool.h>
+#include <sys/wait.h>
+
+/* declaration of functuons */
+void *realloc(void *potr, size_t size_n1, size_n2);
+void *copy_mry(void *adress, const void *srcpy, size_t t);
+size_t get_allocated_size(void *potr);
+char *copy_string(char *destination, const char *source);
+int compare_string(const char *ad, const char *ab);
+size_t get_string_length(const char *s);
+char *concat_stri_stract(char *address, const char *src);
+char *_environ_get(const char *en_var);
+char *get_command(char *command);
+char **string_split(char *buf, const char *deel);
+void comand_exec(char **arguments, char **env);
+void free_token(char **tokens);
+
+#endif
